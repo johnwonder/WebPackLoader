cc7ebd39013cb171665f2e8f1650c93041cdc5ed
johnwonder <johnufida@163.com>
2015-10-16 17:25:38 +0800

    webpack host and port

---
+++ tutorial2/js/app.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Router from 'react-router';
+
+import {DefaultRoute,Link,Route,RouteHandler} from 'react-router';
+
+import HelloHandler from './Hello.js';
+
+let App = React.createClass({
+
+	render(){
+	
+		return (
+		 
+		   <div className="nav">
+			   <Link to="app" className="home">Home</Link>
+			   <Link to="hello" className="hellolink"> Say Hello</Link>
+		{/* this is the important part*/}
+		   <RouteHandler/>
+		</div>
+		);	
+	}
+});
+
+let routes =(
+
+	<Route name="app" path="/" handler={App}>
+	  <Route name="hello" path="/hello" handler={HelloHandler}/>
+	</Route>
+);
+
+Router.run(routes,function(Handler){
+
+   React.render(<Handler/>,document.body);
+
+});
